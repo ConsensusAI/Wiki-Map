5d49d75916acc22971c428652f82813a779d934a
hseiji <henrique.hst@gmail.com>
2022-04-28 20:29:49 -0400

    add seeds

---
+++ db/seeds/04_joins.sql
@@ -0,0 +1,5 @@
+--- joins table ---
+
+INSERT INTO maps_users (user_id, map_id, date_contributed, favourite) VALUES (1, 1, '04/28/2022', 1);
+
+INSERT INTO maps_points (user_id, point_id) VALUES (1, 1);
