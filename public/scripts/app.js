22576505e9dd57a150d0f323aa6472a229784a82
hseiji <henrique.hst@gmail.com>
2022-04-29 21:02:57 -0400

    create add points to map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,28 @@
 // Client facing scripts here
 
+// dummy database
+let user_id = 1;
+let mapObj = {
+  map_id: 1,
+  title: 'Ottawaa',
+  lat: 45.4236237996463,
+  lng: -75.70106847644017,
+  public: true,
+  created_by: 1
+}
+let points = [
+  {
+    lat: 45.4236237996463,
+    lng: -75.70106847644017
+  },
+  {
+    lat: 45.436767,
+    lng: -75.739633
+  }
+]
 //// Map ////
 
-let map = L.map("map").setView([45.4236237996463, -75.70106847644017], 13);
+let map = L.map("map").setView([mapObj.lat, mapObj.lng], 13);
 
 // Tile Layer
 L.tileLayer(
@@ -21,15 +41,16 @@ L.tileLayer(
 
 // Marker
 
-let marker = L.marker([45.4236237996463, -75.70106847644017]).addTo(map);
+let marker = L.marker([points[0].lat, points[0].lng]).addTo(map);
+L.marker([points[1].lat, points[1].lng]).addTo(map);
 
 // Circle
-var circle = L.circle([45.42, -75.7], {
-  color: "red",
-  fillColor: "#f03",
-  fillOpacity: 0.5,
-  radius: 500,
-}).addTo(map);
+// var circle = L.circle([45.42, -75.7], {
+//   color: "red",
+//   fillColor: "#f03",
+//   fillOpacity: 0.5,
+//   radius: 500,
+// }).addTo(map);
 
 // Popups
 
@@ -46,6 +67,19 @@ function onMapClick(e) {
     .setLatLng(e.latlng)
     .setContent("You clicked the map at " + e.latlng)
     .openOn(map);
+
+  if (confirm("Do you want to save this location?")) {
+    L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+    points.push({
+      lat: e.latlng.lat,
+      lng: e.latlng.lng
+    });
+    console.log(points);
+  }
+
+
 }
+
+
 
 map.on("click", onMapClick);
