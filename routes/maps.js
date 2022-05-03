08adaa75800e01d18ac56854f6e50cce0fcd955d
ConsensusAI <malik.iqbal@hotmail.ca>
2022-05-03 19:46:04 -0400

    Rename variables to be more accurate

--- routes/maps.js
+++ routes/maps.js
@@ -7,8 +7,8 @@ const testDb = {};
 router.post("/", function (req, res) {
   testDb["0001"] = {
     name: req.body["newMapName"],
-    latitude: req.body["locationAddress"],
-    longitude: req.body["country"],
+    latitude: req.body["latitude"],
+    longitude: req.body["longitude"],
   };
   res.send(
     "Name: " +
@@ -20,14 +20,14 @@ router.post("/", function (req, res) {
   );
 });
 
-const getMapsByUser = function(email) {
+const getMapsByUser = function (email) {
   return pool
-  .query("SELECT * FROM maps WHERE email = $1", [email])
-  .then((result) => {
-    console.log("maps: ", result.rows[0]);
-    return result.rows[0];
-  })
-  .catch((err) => console.log(err));
+    .query("SELECT * FROM maps WHERE email = $1", [email])
+    .then((result) => {
+      console.log("maps: ", result.rows[0]);
+      return result.rows[0];
+    })
+    .catch((err) => console.log(err));
 };
 
-module.exports = router, getMapsByUser;
+(module.exports = router), getMapsByUser;
