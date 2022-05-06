4f1e7a11efe0db5db21ae97e59544ff0a6faaf26
ConsensusAI <malik.iqbal@hotmail.ca>
2022-05-06 03:33:26 +0000

    Add top margin to url label in new map form

--- public/scripts/newMap.js
+++ public/scripts/newMap.js
@@ -62,7 +62,7 @@ function saveMarker(latlng) {
   <label for="pointDesc">Description: </label>
   <input type="pointDesc" class="form-control" name="pointDesc" placeholder="Best place to eat">
   <div class="form-group">
-  <label for="pointURL">Image URL: </label>
+  <label id="url-label" for="pointURL">Image URL: </label>
   <input type="pointURL" class="form-control" name="pointURL" value=${DEFAULT_IMAGE}>
 </div>
 <button type="submit" class="btn btn-danger">Create Map!</button>
