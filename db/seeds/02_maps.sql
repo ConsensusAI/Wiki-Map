28eb972d0e1532c085c9a554f1cfae0f7ebb3065
hseiji <henrique.hst@gmail.com>
2022-05-07 11:11:55 -0400

    seeds

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,6 +1,6 @@
 --- maps table seeds ---
 
 INSERT INTO maps (title, lat, lng, created_by, public) VALUES ('Best Places in Ottawa', 45.4236237996463, -75.70106847644017, 1, true);
-INSERT INTO maps (title, lat, lng, created_by, public) VALUES ('Restaurants in Montreal', 45.508091, -73.599874, 1, true);
+INSERT INTO maps (title, lat, lng, created_by, public) VALUES ('Food in Montreal', 45.508091, -73.599874, 1, true);
 INSERT INTO maps (title, lat, lng, created_by, public) VALUES ('Must go Toronto', 43.642555, -79.387109, 2, true);
 INSERT INTO maps (title, lat, lng, created_by, public) VALUES ('How to Get to School', 49.281182, -123.114823, 1, true);
