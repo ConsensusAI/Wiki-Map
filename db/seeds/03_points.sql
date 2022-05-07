5877efd04502939365e855fc03d51bd781320d2d
hseiji <henrique.hst@gmail.com>
2022-05-07 00:54:24 -0400

    add seeds

--- db/seeds/03_points.sql
+++ db/seeds/03_points.sql
@@ -1,8 +1,8 @@
 --- maps table seeds ---
 
-INSERT INTO points (map_id, title, description, image, lat, lng, created_by) VALUES (1, 'Parliament of Canada', 'Historic architecture and landscapes with beautiful views of the city and river.', 'http://www.usarice.com/images/default-source/usa-rice-daily-images/international-trade-policy/itp-canadian-parliament-200318.jpg?sfvrsn=db74d68d_4', 45.438217, -75.697346, 1);
-INSERT INTO points (map_id, title, description, image, lat, lng, created_by) VALUES (1, 'Point2', 'Some description here...2', '', 45.436767, -75.739633, 1);
+INSERT INTO points (map_id, title, description, image, lat, lng, created_by) VALUES (1, 'Parliament of Canada', 'Historic architecture and landscapes with beautiful views of the city and river.', 'http://www.usarice.com/images/default-source/usa-rice-daily-images/international-trade-policy/itp-canadian-parliament-200318.jpg?sfvrsn=db74d68d_4', 45.425695, -75.700479, 1);
+INSERT INTO points (map_id, title, description, image, lat, lng, created_by) VALUES (1, 'ByWard Market', 'Some description here...2', '', 45.428115, -75.692368, 1);
 INSERT INTO points (map_id, title, description, image, lat, lng, created_by) VALUES (2, 'Point3', 'Some description here...3', '', 45.508091, -73.599874, 2);
-INSERT INTO points (map_id, title, description, image, lat, lng, created_by) VALUES (1, 'Point4', 'Some description here...4', '', 45.508091, -73.599874, 1);
+INSERT INTO points (map_id, title, description, image, lat, lng, created_by) VALUES (1, 'Victoria Island', 'Some description here...4', '', 45.421947, -75.710853, 1);
 INSERT INTO points (map_id, title, description, image, lat, lng, created_by) VALUES (3, 'CN Tower', 'Most iconic tower of Canada.', 'https://media.istockphoto.com/photos/tower-picture-id483465910', 43.642555, -79.387109, 3);
 INSERT INTO points (map_id, title, description, image, lat, lng, created_by) VALUES (3, 'High Park', 'The High Park is one of the biggest parks in the Toronto region.', 'https://media.istockphoto.com/photos/toronto-postcard-picture-id803293832?s=612x612', 43.645905, -79.463374, 3);
