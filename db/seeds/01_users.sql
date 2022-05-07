d4ef9e6415ed0ecb0053b8bc839c705c6cd7488c
ConsensusAI <malik.iqbal@hotmail.ca>
2022-05-07 05:12:35 +0000

    Add important data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 --- Users table seeds ---
-INSERT INTO users (name, email, password, profile_pic) VALUES ('Alice', 'alice@gmail.com', 'password', 'https://media.istockphoto.com/photos/ginger-cat-picture-id1073475928?k=20&m=1073475928&s=612x612&w=0&h=50Dbbf5cz_-NaS74tLY64PubIWMPTu09DnUnKd8z1ag=');
+INSERT INTO users (name, email, password, profile_pic) VALUES ('Taiwo', 'taiwo@lighthouse.com', 'password', 'https://s3.ca-central-1.amazonaws.com/flex-web-compass-s3bucketcompassapp-6td6wp7knfnw/uploads/avatar/custom_avatar/683/thumb_pic__1_.png');
 INSERT INTO users (name, email, password, profile_pic) VALUES ('George', 'george@gmail.com', 'password', 'https://media.istockphoto.com/photos/ginger-cat-picture-id1073475928?k=20&m=1073475928&s=612x612&w=0&h=50Dbbf5cz_-NaS74tLY64PubIWMPTu09DnUnKd8z1ag=');
 INSERT INTO users (name, email, password, profile_pic) VALUES ('Allan', 'allan@gmail.com', 'password', 'https://media.istockphoto.com/photos/ginger-cat-picture-id1073475928?k=20&m=1073475928&s=612x612&w=0&h=50Dbbf5cz_-NaS74tLY64PubIWMPTu09DnUnKd8z1ag=');
 INSERT INTO users (name, email, password, profile_pic) VALUES ('Jack', 'jack@gmail.com', 'password', 'https://media.istockphoto.com/photos/ginger-cat-picture-id1073475928?k=20&m=1073475928&s=612x612&w=0&h=50Dbbf5cz_-NaS74tLY64PubIWMPTu09DnUnKd8z1ag=');
